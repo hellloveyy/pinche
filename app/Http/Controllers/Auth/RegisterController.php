f530f7b68a648c5475a3a0aaa760738582b47311
hellloveyy <378616976@qq.com>
2017-12-20 22:05:39 +0800

    增加注

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = 'know';
 
     /**
      * Create a new controller instance.
